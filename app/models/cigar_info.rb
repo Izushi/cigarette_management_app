466e6fce5289f9722717c618b128935a80c3041d
Izushi <shoeshinesamurai@gmail.com>
2021-06-14 15:52:10 +0900

    アップロードファイルとcigar_infoモデルの紐付け

--- app/models/cigar_info.rb
+++ app/models/cigar_info.rb
@@ -2,4 +2,6 @@ class CigarInfo < ApplicationRecord
   validates :brand, presence: true, length: { maximum: 50 }
   validates :price, presence: true
   validates :text, length: { maximum: 200 }
+
+  mount_uploader :img, ImgUploader
 end
