f6775a27e5e5216db46ad31eb75ada752765eba6
Izushi <shoeshinesamurai@gmail.com>
2021-06-23 06:04:39 +0900

    ゲストユーザー登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+                       registrations: "users/registrations",
+                       passwords: "users/passwords",
+                     }
   # root "cigar_infos#index"
   root "graphs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :graphs, only: [:index, :create, :update]
   resources :cigar_infos
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end
