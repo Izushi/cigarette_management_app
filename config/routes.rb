2a7630760ae2adc3208d8e24f1f9c339f8c27635
Izushi <shoeshinesamurai@gmail.com>
2021-08-14 01:16:05 +0900

    トップページの作成とfontawesomeを使ったヘッダーの編集

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
                      }
   # root "cigar_infos#index"
   root "graphs#index"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tops
   resource :graphs, only: [:index, :create, :update]
   resources :cigar_infos
   resources :profiles
