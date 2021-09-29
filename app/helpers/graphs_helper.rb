5543b6051d9510ad964f634c2dd3f00026fb812e
Izushi <shoeshinesamurai@gmail.com>
2021-09-29 17:43:51 +0900

    graphs_helperを編集

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,16 +1,19 @@
 module GraphsHelper
   def month
+    # parseメソッドで文字列DateクラスをDateオブジェクトに変換
+    # 今日を含む月を取得
     @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
   end
 
+  # 取得した時刻が含まれる月の範囲のgraphsテーブルのデータを取得
   def cigarettes_this_month
-    @cigarettes_this_month = current_user.graphs.where(date: month.all_month)
+    current_user.graphs.where(date: month.all_month)
   end
 
+  # 月の合計喫煙本数
   def sum_cigarette_this_month
     sum = 0
-    cigarettes_this_month
-    @cigarettes_this_month.each { |n| sum += n.cigarette }
+    cigarettes_this_month.each { |n| sum += n.cigarette }
     sum
   end
 end
